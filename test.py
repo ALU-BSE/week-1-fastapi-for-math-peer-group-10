8e3bd292a130bc97ebcc1a895e1b815c7f081f59
NicolleNM <138711504+NicolleNM@users.noreply.github.com>
2025-01-10 17:58:38 +0200

    Host defined

--- test.py
+++ test.py
@@ -45,7 +45,7 @@ def calculate(input: MatrixInput):
     }
 
 if __name__ == "__main__":
-    uvicorn.run(app)
+    uvicorn.run(app, host="127.0.0.1", port=8000)
 
 '''
     Create a requirements.txt
