215702937f35fe929255b2fc249c002e808c1471
NicolleNM <138711504+NicolleNM@users.noreply.github.com>
2025-01-10 16:55:11 +0200

    Initialised variables and defined sigmoid

--- test.py
+++ test.py
@@ -1,13 +1,23 @@
 from fastapi import FastAPI
 import uvicorn 
 import numpy as np 
+import math
+from pydantic import BaseModel
 
 app = FastAPI()
 
-# use the post decorator directly below this
-'''
-    Initialize M and B as np arrays
-'''
+class Matrix(BaseModel):
+    matrix: list
+ 
+#Initialise M & B as variable
+M = np.random.rand(5,5)
+B = np.random.rand(5,5)
+
+def sigmoid(x):
+    return 1 / (1 + math.exp(-x))
+
+
+
 def f(x):
     pass
  
